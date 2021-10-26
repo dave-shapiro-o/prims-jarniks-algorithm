226493a15411d21caaef2fce102d08b631ae6993
nastydave <dave-shapiro-o>
2021-10-26 23:23:46 +0100

    added comments!

--- src/Result.java
+++ src/Result.java
@@ -13,12 +13,12 @@ class Result {
      *  2. 2D_INTEGER_ARRAY edges
      *  3. INTEGER start
      */
-
     public static int prims(int n, List<List<Integer>> edges, int start) {
 
         int totalWeight = 0;
         int edgeCount = 0;
 
+        // Nodes added to the MST by setting == true in "includedNodes" array.
         boolean[] includedNodes = new boolean[n+1];
         Arrays.fill(includedNodes, false);
         includedNodes[start] = true;
@@ -27,6 +27,9 @@ class Result {
             int minEdge = Integer.MAX_VALUE;
             int minEdgeIndex = 0;
 
+            // For each of the edges remaining in the list,
+            // if one of its nodes are in the MST and the other is not,
+            // and if it is the cheapest edge thus far, mark it as such.
             for (int i = 0; i < edges.size(); ++i){
                 List<Integer> current = edges.get(i);
                     if ((includedNodes[current.get(0)] && !includedNodes[current.get(1)])
@@ -39,6 +42,8 @@ class Result {
                 }
 
             List<Integer> selectedEdge = edges.get(minEdgeIndex);
+            // Add the new node to the MST, add its weight to the total,
+            // increment the edge count and remove the edge from the list of candidates ("edges").
             if (includedNodes[selectedEdge.get(0)]) { includedNodes[selectedEdge.get(1)] = true; }
             else { includedNodes[selectedEdge.get(0)] = true; }
             totalWeight += minEdge;
